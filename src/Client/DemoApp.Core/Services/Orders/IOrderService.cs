ebb350ab4bb95ba2f0eba26f96921d67b06e0be7
David Martínek <david.martinek@plus4u.net>
2020-10-18 21:18:39 +0200

    OrderService

---
+++ src/Client/DemoApp.Core/Services/Orders/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using DemoApp.Core.Models.Orders;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DemoApp.Core.Services.Orders
+{
+    public interface IOrderService
+    {
+        Task<List<OrderDto>> GetAllOrdersAsync();
+    }
+}
---
+++ src/Client/DemoApp.Core/Services/Orders/OrderService.cs
@@ -0,0 +1,46 @@
+﻿using DemoApp.Core.Config;
+using DemoApp.Core.Models.Orders;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DemoApp.Core.Services.Orders
+{
+    public class OrderService : IOrderService
+    {
+        private const string HttpClientName = "UserServiceHttpClient";
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly AppSettings _appSettings;
+
+        public OrderService(IOptions<AppSettings> appSettings, IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+            _appSettings = appSettings.Value;
+        }
+
+        public async Task<List<OrderDto>> GetAllOrdersAsync()
+        {
+            var client = this.GetClient();
+            var response = await client.GetAsync("api/orders");
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("User service not available.");
+            }
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            return JsonSerializer.Deserialize<List<OrderDto>>(result);
+        }
+
+        private HttpClient GetClient()
+        {
+            var client = _clientFactory.CreateClient(HttpClientName);
+            client.BaseAddress = new Uri(_appSettings.Services.UserServiceUrl);
+
+            return client;
+        }
+    }
+}
